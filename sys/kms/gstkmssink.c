a435786dc58cc17d298b8d87175286d69dc90d7b
Haihua Hu <jared.hu@nxp.com>
2017-08-08 12:55:42 +0800

    kmssink: use control node to setplane to avoid getting DRM-Master
    a workaround on Linux 4.9 for kmssink multi-user
    
    upstream status: i.mx specific
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>
    (cherry picked from commit 87a0a126882d70e262ba5d452af82b75050cb111)

--- sys/kms/gstkmssink.c
+++ sys/kms/gstkmssink.c
@@ -56,6 +56,8 @@
 #include <xf86drmMode.h>
 #include <drm_fourcc.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include "gstkmssink.h"
 #include "gstkmsutils.h"
@@ -594,6 +596,21 @@ ensure_allowed_caps (GstKMSSink * self, drmModeConnector * conn,
   return TRUE;
 }
 
+static gint
+get_drm_minor_base (gint type)
+{
+  switch (type) {
+    case DRM_NODE_PRIMARY:
+      return 0;
+    case DRM_NODE_CONTROL:
+      return 64;
+    case DRM_NODE_RENDER:
+      return 128;
+    default:
+      return -1;
+  }
+}
+
 static gboolean
 set_drm_property (gint fd, guint32 object, guint32 object_type,
     drmModeObjectPropertiesPtr properties, const gchar * prop_name,
@@ -729,6 +746,7 @@ gst_kms_sink_start (GstBaseSink * bsink)
   drmModePlane *plane;
   gboolean universal_planes;
   gboolean ret;
+  gint minor;
 
   self = GST_KMS_SINK (bsink);
   universal_planes = FALSE;
@@ -743,7 +761,11 @@ gst_kms_sink_start (GstBaseSink * bsink)
     self->fd = drmOpen (self->devname, self->bus_id);
   else
     self->fd = kms_open (&self->devname);
-  if (self->fd < 0)
+
+  minor = get_drm_minor_base (DRM_NODE_CONTROL);
+  self->ctrl_fd = drmOpenControl(minor);
+
+  if (self->fd < 0 || self->ctrl_fd < 0)
     goto open_failed;
 
   log_drm_version (self);
@@ -957,6 +979,11 @@ gst_kms_sink_stop (GstBaseSink * bsink)
   if (self->fd >= 0) {
     drmClose (self->fd);
     self->fd = -1;
+  }
+
+  if (self->ctrl_fd >= 0) {
+    drmClose (self->ctrl_fd);
+    self->ctrl_fd = -1;
   }
 
   GST_OBJECT_LOCK (bsink);
@@ -1697,7 +1724,7 @@ retry_set_plane:
       "drmModeSetPlane at (%i,%i) %ix%i sourcing at (%i,%i) %ix%i",
       result.x, result.y, result.w, result.h, src.x, src.y, src.w, src.h);
 
-  ret = drmModeSetPlane (self->fd, self->plane_id, self->crtc_id, fb_id, 0,
+  ret = drmModeSetPlane (self->ctrl_fd, self->plane_id, self->crtc_id, fb_id, 0,
       result.x, result.y, result.w, result.h,
       /* source/cropping coordinates are given in Q16 */
       src.x << 16, src.y << 16, src.w << 16, src.h << 16);
