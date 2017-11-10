75007dd3112f6d289e93f9da7e720fc096d5d911
Haihua Hu <jared.hu@nxp.com>
2017-11-10 19:52:47 +0800

    kmssink: fix performance issue by bypass frame sync
    drm setplane will block on vblank before return.
    it’s a sync call. So no need do frame sync in kmssink.

--- sys/kms/gstkmssink.c
+++ sys/kms/gstkmssink.c
@@ -1708,7 +1708,8 @@ retry_set_plane:
       goto retry_set_plane;
     }
     goto set_plane_failed;
-  }
+  } else
+    goto done;
 
 sync_frame:
   /* Wait for the previous frame to complete redraw */
@@ -1717,6 +1718,7 @@ sync_frame:
     goto bail;
   }
 
+done:
   if (buffer != self->last_buffer)
     gst_buffer_replace (&self->last_buffer, buffer);
   g_clear_pointer (&self->tmp_kmsmem, gst_memory_unref);
