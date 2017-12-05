060b3ba39e0365beaf2da1e18c2275714e5b6250
Haihua Hu <jared.hu@nxp.com>
2017-12-05 16:25:14 +0800

    only check scale once per playback
    upstream status: imx specific
    
    Signed-off-by: Haihua Hu <jared.hu@nxp.com>

--- sys/kms/gstkmssink.c
+++ sys/kms/gstkmssink.c
@@ -431,7 +431,10 @@ check_scaleable (GstKMSSink * self)
   guint32 crtc_w, crtc_h;
   GstKMSMemory *kmsmem = NULL;
 
-  if (!self->can_scale)
+  /* we assume driver can scale at initialize,
+   * if scale is checked or can not scale, we
+   * don't need check again */
+  if (self->scale_checked || !self->can_scale)
     return;
 
   if (self->conn_id < 0)
@@ -458,6 +461,7 @@ check_scaleable (GstKMSSink * self)
     GST_INFO_OBJECT (self, "scale is not support");
   }
 
+  self->scale_checked = TRUE;
   g_clear_pointer (&kmsmem, gst_memory_unref);
 }
 
@@ -2087,6 +2091,7 @@ gst_kms_sink_init (GstKMSSink * sink)
   sink->conn_id = -1;
   sink->plane_id = -1;
   sink->can_scale = TRUE;
+  sink->scale_checked = FALSE;
   gst_poll_fd_init (&sink->pollfd);
   sink->poll = gst_poll_new (TRUE);
   gst_video_info_init (&sink->vinfo);
