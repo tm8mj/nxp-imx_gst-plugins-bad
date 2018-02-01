c243a18c9f1d931a7dc6db292ca9963d4db61c39
Lyon Wang <lyon.wang@nxp.com>
2018-02-01 11:37:34 +0800

    Update for gplay not set video-sink by default
    MMFMWK-7883
    - Add video-direction property in set-rotate
    - Update get video-sink for autoplug video-sink case
    - Add kmssink priority logic
    - kmssink include gstimxcommon.h
    
    Signed-off-by: Lyon Wang <lyon.wang@nxp.com>

--- gst-libs/gst/player/gstplayer.c
+++ gst-libs/gst/player/gstplayer.c
@@ -4859,6 +4859,8 @@ gst_player_get_video_sink (GstPlayer * self)
     GST_WARNING_OBJECT (self, "No video-sink found");
     return NULL;
   }
+  if (TRUE != GST_IS_BIN((GstBin*) sink))
+     return sink;
   it = gst_bin_iterate_sinks ((GstBin *) sink);
   do {
     rc = gst_iterator_next (it, &item);
@@ -4910,6 +4912,9 @@ gst_player_set_rotate (GstPlayer * self, gint rotation)
     g_object_set (G_OBJECT (video_sink), "reconfig", 1, NULL);
   } else if (g_object_class_find_property (gobjclass, "rotate-method")) {
     g_object_set (G_OBJECT (video_sink), "rotate-method", rotation / 90, NULL);
+  }  else if (g_object_class_find_property (gobjclass, "video-direction")) {
+    g_object_set (G_OBJECT (video_sink), "video-direction", rotation / 90, NULL);
+    g_object_set (G_OBJECT (video_sink), "reconfig", 1, NULL);
   } else {
     GST_INFO_OBJECT (self, "can't set rotation for current video sink %s'",
         gst_element_get_name (video_sink));
@@ -4946,6 +4951,9 @@ gst_player_get_rotate (GstPlayer * self)
     rotation = rotation * 90;
   } else if (g_object_class_find_property (gobjclass, "rotate-method")) {
     g_object_get (G_OBJECT (video_sink), "rotate-method", &rotation, NULL);
+    rotation = rotation * 90;
+  }   else if (g_object_class_find_property (gobjclass, "video-direction")) {
+    g_object_get (G_OBJECT (video_sink), "video-direction", &rotation, NULL);
     rotation = rotation * 90;
   }
 
