59accdc763ff9be90cbc489f387f6df5d0b63bc8
Haihua Hu <jared.hu@nxp.com>
2018-07-05 19:03:48 +0800

    MMFMWK-7954 waylandsink: apply surface buffer scale for 850D
    get real display solution using wl_output interface. Compare it
    with desktop size to decide which scale to use. defaultly, make
    scale = 1 for compitable purpose.
    
    upstream status: imx specific
    
    Conflicts:
    	ext/wayland/wldisplay.c
    	ext/wayland/wlwindow.c

--- ext/wayland/wldisplay.h
+++ ext/wayland/wldisplay.h
@@ -60,12 +60,16 @@ struct _GstWlDisplay
   struct xdg_wm_base *xdg_wm_base;
   struct zwp_fullscreen_shell_v1 *fullscreen_shell;
   struct wl_shm *shm;
+  struct wl_output *output;
   struct wp_viewporter *viewporter;
   struct zwp_linux_dmabuf_v1 *dmabuf;
   struct zwp_alpha_compositing_v1 *alpha_compositing;
   struct zwp_hdr10_metadata_v1 *hdr10_metadata;
   GArray *shm_formats;
   GArray *dmabuf_formats;
+
+  /* real display resolution */
+  gint width, height;
 
   /* private */
   gboolean own_display;
