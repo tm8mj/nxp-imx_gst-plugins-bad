6efedb09f8e48d54c0b32ee551a095df5386bccf
Hou Qi <qi.hou@nxp.com>
2024-04-22 14:29:50 +0800

    MMFMWK-9362 wlwindow: free staged buffer when do gst_wl_window_finalize
    If waylandsink received buffer rate is high which causes frame
    drop, the cached staged buffer will be replaced when next buffer
    needs to be rendered and be freed after redraw. But there is
    chance to get memory leak if ended without redraw. So need to
    free staged buffer when do gst_wl_window_finalize().
    
    Upstream status: pending
    Part-of: <https://gitlab.freedesktop.org/gstreamer/gstreamer/-/merge_requests/6670>

--- gst-libs/gst/wayland/gstwlwindow.c
+++ gst-libs/gst/wayland/gstwlwindow.c
@@ -409,6 +409,9 @@ gst_wl_window_finalize (GObject * gobject)
   gst_wl_display_callback_destroy (priv->display, &priv->frame_callback);
   gst_wl_display_callback_destroy (priv->display, &priv->commit_callback);
 
+  if (priv->staged_buffer)
+    gst_wl_buffer_unref_buffer (priv->staged_buffer);
+
   if (priv->xdg_toplevel)
     xdg_toplevel_destroy (priv->xdg_toplevel);
   if (priv->xdg_surface)
